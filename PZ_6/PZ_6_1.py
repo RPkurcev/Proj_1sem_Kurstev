4081aa77c68657449ea3225e939916a609dabdd4
386 <romankurcev4@gmail.com>
2021-11-30 00:40:21 +0300

    PZ_6

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,12 +1,12 @@
 # Алгоритм должен выводить элементы из списка (список не имеет ограничений) с чётными номерами(индексом).
-import random
-from random import ran
-A = [1, 3, 2, 2, 9]
-print("Спиок A = ", A)
 
-z = len(A)
-s = 0
+A = list(range(-10, 10))
+s = len(A)
+print("Список A = ", A)
+
 k = 2
-while s < z:
-    print(A[k])
-    k += 2
+while k < s:
+    while k < s:
+        print("Элемент", A[k], "под номером", k)
+        k += 2
+        break
