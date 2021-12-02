c7228df72f28ffe8b909001539be46dbde5a2dcc
Student <romankurcev4@gmail.com>
2021-12-02 16:30:50 +0300

    PZ_7

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -2,6 +2,8 @@
 # (без символа "\"), если файл содержится в корневом каталоге, то вывесть символ "\"
 name_f = r'C:\Users\Student\PycharmProjects\POKS-22'
 
+symbol = name_f[1:3]
+
 d = 0
 for w in name_f:
     d += 1
@@ -9,12 +11,9 @@ for w in name_f:
         d -= 1
         break
 
-
 for n in name_f:
-    if n == ":" or n == '\\':
-        d += 1
+    if n == ':' and '\\':
         for n in name_f:
-            d += 1
-        print(name_f[d], end='')
-        if n == '\\':
-            break
+            print(n, end='')
+            if n == '\\':
+                break
