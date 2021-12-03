a72e9ce18555018630d75c85a231bcee06219dc0
386 <romankurcev4@gmail.com>
2021-12-03 23:00:04 +0300

    PZ_7

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -2,19 +2,16 @@
 # (без символа "\"), если файл содержится в корневом каталоге, то вывесть символ "\"
 name_f = r'C:\Users\Student\PycharmProjects\POKS-22'
 
+name_f1 = name_f[3:]  # Избавляемся от C:\, так как оно будет мещать при итерации.
+
 d = 0
-for w in name_f:
-    d += 1
-    if w == ':':
-        d -= 1
-        break
-
-
-for n in name_f:
-    if n == ":" or n == '\\':
+for el in name_f1:
+    while d < 1:  # Цикл позволет вывести слово одние раз.
         d += 1
-        for n in name_f:
-            d += 1
-        print(name_f[d], end='')
-        if n == '\\':
-            break
+        if '\\' in name_f1:
+            for el in name_f1:
+                if el == '\\':
+                    break
+                print(el, end='')
+        else:
+            print('\\ - имя файла.')
