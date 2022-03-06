134acce3929df988b93dfacd9a4ffa9179b3f091
386 <romankurcev4@gmail.com>
2022-03-06 14:44:49 +0300

    PZ_14

--- PZ_14/PZ_14.py
+++ PZ_14/PZ_14.py
@@ -6,13 +6,24 @@ import re
 
 with open("Dostoevsky.txt", 'r', encoding='utf-8') as file:
     text = file.readlines()
+
     b = False
+    strings = []
     for n in text:
-        if n == "1857 год \n":
+        if n == "1857 год \n": # Данный блок условного оператора нужен для поиска нужного блока информации
             b = True
         elif n == "1860–1866 гг.\n":
             b = False
 
         while b:
-            print(*re.findall(r".*", n))
+            strings += re.findall(r".*", n)
+            strings.append("\n")
             break
+
+with open("text_1857.txt", 'a', encoding='utf-8') as f:
+    f.writelines(strings)
+
+
+
+
+
