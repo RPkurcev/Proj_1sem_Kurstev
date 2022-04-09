a5ab507c170c864c6bfa155f297b02d4e75e4ad0
386 <romankurcev4@gmail.com>
2022-04-09 22:02:45 +0300

    Раздаточный материал №69

--- DB/__init__.py
+++ DB/__init__.py
@@ -3,22 +3,32 @@ import sqlite3 as sq
 with sq.connect('saper.db') as con:
     cur = con.cursor()
 
-    cur.execute("""SELECT * FROM users""") # Выбираем всех игроков из таблицы
+    # Выбираем всех игроков из таблицы
+    cur.execute("""SELECT * FROM users""")
     result = cur.fetchall()
     print(result)
 
-    cur.execute("""SELECT * FROM users WHERE sex == 2""")  # Выбираем всех игроков женского пола
+    # Выбираем всех игроков женского пола
+    cur.execute("""SELECT * FROM users WHERE sex == 2""")
     result = cur.fetchall()
     print(result)
 
-    cur.execute("""SELECT * FROM users WHERE score < 1000""")  # Выбираем всех игроков у которых результат меньше 1000
+    # Выбираем всех игроков у которых результат меньше 1000
+    cur.execute("""SELECT * FROM users WHERE score < 1000""")
     result = cur.fetchall()
     print(result)
 
-    cur.execute("""SELECT * FROM users WHERE score > 1000""")  # Выбираем всех игроков с наилучшим результатом
+    # Выбираем всех игроков с наилучшим результатом
+    cur.execute("""SELECT * FROM users WHERE score > 1000""")
     result = cur.fetchall()
     print(result)
 
-    cur.execute("""SELECT * FROM users WHERE old 18 BETWEEN 18 AND 20""")  # Выбираем всех игроков с наилучшим результатом
+    # Выбираем всех игроков из таблицы с возрастом 18-20 лет
+    cur.execute("""SELECT * FROM users WHERE old BETWEEN 18 AND 20""")
     result = cur.fetchall()
     print(result)
+
+
+
+
+
