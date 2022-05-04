bd4eba98ec86b3432cf913e0cd2e8a9db228ecac
Merel5 <romankurcev4@gmail.com>
2022-05-04 18:06:06 +0300

    Сапёр

---
+++ Saper/Update.py
@@ -0,0 +1,20 @@
+from Child import *
+import tkinter as tk
+from tkinter import ttk
+
+class Update(Child):
+    def __init__(self):
+        super().__init__(root, app)
+        self.init_edit()
+        self.view = app
+
+    def init_edit(self):
+        self.title("Редактировать запись")
+        btn_edit = ttk.Button(self, text="Редактировать")
+        btn_edit.place(x=205, y=170)
+        btn_edit.bind('<Button-1>', lambda event: self.view.update_record(self.entry_description.get(),
+                                                                          self.entry_name.get(),
+                                                                          self.combobox.get(),
+                                                                          self.entry_old.get(),
+                                                                          self.entry_score.get()))
+        self.btn_ok.destroy()
