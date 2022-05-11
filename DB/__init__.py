6d5ae0384bcfcb6950d8afaca58510bcf62c6afc
Merel5 <romankurcev4@gmail.com>
2022-05-11 18:08:24 +0300

    Сапёр

--- DB/__init__.py
+++ DB/__init__.py
@@ -3,6 +3,7 @@ import sqlite3 as sq
 with sq.connect('saper.db') as con:
     cur = con.cursor()
 
+
     # Выбираем всех игроков из таблицы
     cur.execute("""SELECT * FROM users""")
     result = cur.fetchall()
